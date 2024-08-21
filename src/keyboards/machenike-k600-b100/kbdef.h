61eaa91212955746504f22b4484a67b6a9acb0ea
I-AM-ENGINEER <sdwad6@gmail.com>
2024-08-21 20:56:40 +0300

    Add k600 layout

---
+++ src/keyboards/machenike-k600-b100/kbdef.h
@@ -0,0 +1,67 @@
+#ifndef KBDEF_H
+#define KBDEF_H
+
+#include "sh68f90a.h"
+
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 19
+
+// Row Pins Bits
+#define KB_R0_P7_1 _P7_0
+#define KB_R1_P7_1 _P7_1
+#define KB_R2_P7_2 _P7_2
+#define KB_R3_P7_3 _P7_3
+#define KB_R4_P5_3 _P5_3
+#define KB_R5_P5_4 _P5_4
+
+// Row Pins
+#define KB_R0 P7_0
+#define KB_R1 P7_1
+#define KB_R2 P7_2
+#define KB_R3 P7_3
+#define KB_R4 P5_3
+#define KB_R5 P5_4
+
+// Column Pins Bits
+#define KB_C0_P5_0 _P5_0
+#define KB_C1_P5_1 _P5_1
+#define KB_C2_P5_2 _P5_2
+#define KB_C3_P3_5 _P3_5
+#define KB_C4_P3_4 _P3_4
+#define KB_C5_P3_3 _P3_3
+#define KB_C6_P3_2 _P3_2
+#define KB_C7_P3_1 _P3_1
+#define KB_C8_P3_0 _P3_0
+#define KB_C9_P2_5 _P2_5
+#define KB_C10_P2_4 _P2_4
+#define KB_C11_P2_3 _P2_3
+#define KB_C12_P2_2 _P2_2
+#define KB_C13_P2_1 _P2_1
+#define KB_C14_P2_0 _P2_0
+#define KB_C15_P1_5 _P1_5
+#define KB_C13_P1_4 _P1_4
+#define KB_C14_P1_3 _P1_3
+#define KB_C15_P1_2 _P1_2
+
+// Column Pins
+#define KB_C0 P5_0
+#define KB_C1 P5_1
+#define KB_C2 P5_2
+#define KB_C3 P3_5
+#define KB_C4 P3_4
+#define KB_C5 P3_3
+#define KB_C6 P3_2
+#define KB_C7 P3_1
+#define KB_C8 P3_0
+#define KB_C9 P2_5
+#define KB_C10 P2_4
+#define KB_C11 P2_3
+#define KB_C12 P2_2
+#define KB_C13 P2_1
+#define KB_C14 P2_0
+#define KB_C15 P1_5
+#define KB_C13 P1_4
+#define KB_C14 P1_3
+#define KB_C15 P1_2
+
+#endif
